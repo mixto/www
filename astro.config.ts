32ae919c287e79a99d63f32725905d9d3c226650
human <326143391+mixto@users.noreply.github.com>
2026-09-13 22:18:35 +0200

    astro check

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,18 @@
 // @ts-check
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig, fontProviders } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://example.org',
+  integrations: [sitemap()],
   vite: { plugins: [tailwindcss()] },
   fonts: [
     {
-      cssVariable: '--font-jetbrains-mono',
-      name: 'JetBrains Mono',
-      provider: fontProviders.google()
+      cssVariable: '--font-iosevka',
+      name: 'Iosevka',
+      provider: fontProviders.fontsource()
     }
   ]
 })
